9df2b793ff4f828ecc1c759ed994de29049204ef
jittergtz <sandro.gantze@icloud.com>
2024-01-20 04:34:31 +0100

    dashboard v.0.5.2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import Header from '@/components/Header'
+import ListItem from '@/components/ListItem'
+
 import React from 'react'
 
 function page() {
@@ -6,8 +8,32 @@ function page() {
     <main className='bg-white bg-opacity-5  rounded-lg h-full 
     w-full overflow-hidden overflow-y-auto'>
       <Header>
-        Header
+        <div className='mb-2'>
+          <h1 className='text-neutral-400 text-2xl font-semibold'>
+            Willkommen 
+          </h1>
+          <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
+          gap-3 mt-4'>
+            <ListItem 
+            name="Favoriten"
+            image="/image/volume-heartIcon.svg"
+             href='liked' />
+          </div>
+
+        </div>
+        
       </Header>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-between items-center' >
+          <h1 className='text-white text-2xl font-semibold'>
+            Neue Songs
+          </h1>
+        </div>
+        <div>
+          Liste der Songs!
+        </div>
+
+      </div>
       
     
      
