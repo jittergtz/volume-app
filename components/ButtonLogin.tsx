a24e4a90e8f82522632109a49fcb4f7a5dadb5a1
jittergtz <sandro.gantze@icloud.com>
2024-01-23 19:03:33 +0100

    fix 0.3

--- components/ButtonLogin.tsx
+++ components/ButtonLogin.tsx
@@ -1,31 +1,25 @@
-import { Button, button } from '@nextui-org/react'
-import React, { forwardRef } from 'react'
 
+import React, { forwardRef } from 'react';
 
-interface ButtonLoginProps 
-    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonLoginProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-
-const ButtonLogin = forwardRef<HTMLButtonElement, ButtonLoginProps>(({
-    className,
-    children,
-    disabled,
-    type = "button",
-    ...props
-}, ref) => {
+const ButtonLogin = forwardRef<HTMLButtonElement, ButtonLoginProps>(
+  ({ className, children, disabled, type = 'button', ...props }, ref) => {
     return (
-        <button 
+      <button
         type={type}
         disabled={disabled}
         ref={ref}
         {...props}
         color='primary'
-        className='rounded-full' >
-            Anmelden
-        </button>
-    )
-})
+        className='rounded-full'
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
-Button.displayName = "Button"
+ButtonLogin.displayName = 'ButtonLogin';
 
-export default ButtonLogin
+export default ButtonLogin;
