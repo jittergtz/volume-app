6b5839f9a063a86d465ba76cfffe18479620281c
jittergtz <sandro.gantze@icloud.com>
2024-01-23 18:52:49 +0100

    fix auth

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,35 +1,14 @@
-"use client"
 
+import AuthCard from '@/components/AuthCard'
 import Header from '@/components/Header'
 import MobileNavbar from '@/components/MobileNav'
-import useAuthModal from '@/hooks/useAuthModal'
-import { useUser } from '@/hooks/useUser'
-import { Button } from '@nextui-org/react'
-import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
-import { useRouter } from 'next/navigation'
 import React from 'react'
-import toast from 'react-hot-toast'
+
 
 
 function page() {
-    const router = useRouter()
-    const authModal = useAuthModal()
 
-    const supabaseClient = useSupabaseClient()
-    const { user } = useUser()
-
-    const handleLogout = async () => {
-      const { error } = await supabaseClient.auth.signOut()
-        //Todo : reset any playing song
-      router.refresh()
-
-      if(error){
-        toast.error(error.message)
-      } else {
-        toast.success("Ausgeloggt")
-      }
-    }
 
 
   return (
@@ -49,34 +28,10 @@ function page() {
    
 
         
-
+        <AuthCard/>
        
        
-        <div className=' flex p-5 w-full  justify-center '>
 
-        { user ? (
-                  <div className='flex  items-center'>
-                   <Button 
-                   onClick={handleLogout}
-                   className='rounded-full  px-20 w-full  '>
-                    Ausloggen
-                   </Button>
-                  </div>
-               ): (
-               <>
-                <div>
-                <Button
-                color='primary'
-                className='rounded-full  px-20  '
-                onClick={authModal.onOpen}>
-                    Anmelden
-                </Button>
-                 </div>
-            
-                </>
-               )}
-
-</div>
 
 
 
---
+++ components/AuthCard.tsx
@@ -0,0 +1,61 @@
+"use client"
+import useAuthModal from '@/hooks/useAuthModal'
+import { useUser } from '@/hooks/useUser'
+import { Button } from '@nextui-org/react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import toast from 'react-hot-toast'
+
+
+
+
+
+function AuthCard() {
+    const router = useRouter()
+    const authModal = useAuthModal()
+
+    const supabaseClient = useSupabaseClient()
+    const { user } = useUser()
+
+    const handleLogout = async () => {
+      const { error } = await supabaseClient.auth.signOut()
+        //Todo : reset any playing song
+      router.refresh()
+
+      if(error){
+        toast.error(error.message)
+      } else {
+        toast.success("Ausgeloggt")
+      }
+    }
+  return (
+    <div className=' flex p-5 w-full  justify-center '>
+
+    { user ? (
+              <div className='flex  items-center'>
+               <Button 
+               onClick={handleLogout}
+               className='rounded-full  px-20 w-full  '>
+                Ausloggen
+               </Button>
+              </div>
+           ): (
+           <>
+            <div>
+            <Button
+            color='primary'
+            className='rounded-full  px-20  '
+            onClick={authModal.onOpen}>
+                Anmelden
+            </Button>
+             </div>
+        
+            </>
+           )}
+
+</div>
+  )
+}
+
+export default AuthCard
