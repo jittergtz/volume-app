816fecee9b7afa12a0d3bebd2312c74093773047
jittergtz <sandro.gantze@icloud.com>
2024-01-30 20:56:19 +0100

    fix typo

--- components/Next-Ui/GridImgLp.tsx
+++ components/Next-Ui/GridImgLp.tsx
@@ -81,7 +81,7 @@ export default function GridImgLp() {
           />
           <div className="flex flex-col">
             <p className="text-tiny text-white/60">Breathing App</p>
-            <p className="text-tiny text-white/60">Get a good night's sleep.</p>
+            <p className="text-tiny text-white/60">Get a good nights sleep.</p>
           </div>
         </div>
         <Button radius="full" size="sm">Get App</Button>
--- components/marketing/NavbarLp.tsx
+++ components/marketing/NavbarLp.tsx
@@ -8,7 +8,7 @@ function Navbar() {
     <nav className="h-16 z-50 flex px-5 justify-between items-center fixed top-0 left-0 w-full  backdrop-blur-sm">
       <span className="text-3xl tracking-tighter text-neutral-300">Volume</span>
 
-      <Button className="bg-indigo-500/20 border-2 border-indigo-800 " 
+      <Button className="bg-transparent border-2 border-neutral-100/30 " 
             variant="shadow">
       <Link href={"/"}>
       Anmelden
