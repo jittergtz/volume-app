4ec20655e4fcef40b8cf2d191d2d0dac90e9694d
jittergtz <sandro.gantze@icloud.com>
2024-03-19 00:42:59 +0100

    fix deploy #2

--- components/marketing/TextReveal.tsx
+++ components/marketing/TextReveal.tsx
@@ -18,20 +18,24 @@ function TextReveal({ text, color }: TextRevealProps) {
     paragraph.innerHTML = words.map(word => `<span>${word}</span>`).join(' ');
 
     let spans: NodeListOf<Element>;
-
+  
+  
     const updateSpans = () => {
-      spans = paragraph.querySelectorAll("span");
-      spans.forEach(span => {
+      const spanElements = Array.from(paragraph.querySelectorAll<HTMLSpanElement>("span"));
+      spanElements.forEach(span => {
         const rect = span.parentElement?.getBoundingClientRect();
         if (rect && rect.top < window.innerHeight / 0) {
           let { left, top } = span.getBoundingClientRect();
           top = top - (window.innerHeight * .7);
-          let opacityValue = 1 - ((top * .01) + (left * 0.001)) < 0.1 ? 0.1 : 1 - ((top * .01) + (left * 0.001)).toFixed(3);
-          opacityValue = opacityValue > 1 ? 1 : Number(opacityValue.toFixed(3));
-          span.style.opacity = String(opacityValue);
+          let opacityValue = 1 - ((top * .01) + (left * 0.001));
+          opacityValue = opacityValue < 0.1 ? 0.1 : opacityValue;
+          opacityValue = opacityValue > 1 ? 1 : opacityValue;
+          span.style.opacity = opacityValue.toFixed(3);
         }
       });
     };
+    
+    
 
     updateSpans();
     window.addEventListener("scroll", updateSpans);
