784f5d36f79c80fe397269d0e55dc61aaf372d99
jittergtz <sandro.gantze@icloud.com>
2024-03-19 20:19:43 +0100

    ambient color change

--- components/Next-Ui/dashboardAmbientLight.tsx
+++ components/Next-Ui/dashboardAmbientLight.tsx
@@ -6,7 +6,7 @@ function DashboardAmbientLight() {
       
 
        <div className='absolute top-5 right-3 w-screen h-48 
-       bg-gradient-to-b from-blue-800/50 blur-2xl'>
+       bg-gradient-to-b from-orange-400/40 blur-2xl'>
 
        </div>
     </>
