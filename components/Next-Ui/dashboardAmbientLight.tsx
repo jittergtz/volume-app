3cde4a8a9989f2ebf5370570b00d987bedc19017
jittergtz <sandro.gantze@icloud.com>
2024-03-19 20:23:10 +0100

    #2

--- components/Next-Ui/dashboardAmbientLight.tsx
+++ components/Next-Ui/dashboardAmbientLight.tsx
@@ -6,7 +6,7 @@ function DashboardAmbientLight() {
       
 
        <div className='absolute top-5 right-3 w-screen h-48 
-       bg-gradient-to-b from-orange-400/40 blur-2xl'>
+       bg-gradient-to-b from-orange-700/30 blur-xl'>
 
        </div>
     </>
