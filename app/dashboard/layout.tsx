73bc52e27d8b2b9a744b3845597cd28434c77d2d
jittergtz <sandro.gantze@icloud.com>
2024-03-19 03:41:24 +0100

    add PWA

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,10 +8,32 @@ import ModalProvider from "@/providers/ModalProvider"
 import SupabaseProvider from "@/providers/SupabaseProvider"
 import ToasterProvider from "@/providers/ToasterProvider"
 import UserProvider from "@/providers/UserProvider"
+import { Metadata } from "next"
 
 interface SidebarProps {
   children: React.ReactNode
 }
+
+// Metadatenexport
+export const metadata: Metadata = {
+  title: "Volume",
+  description: "PWA application with Next 13",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  
+  authors: [
+    { name: "Sandro Gantze" }
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/manifest-icon-192.maskable.png" },
+    { rel: "icon", url: "icons/manifest-icon-512.maskable.png" },
+  ],
+};
+
+// Viewport export
+export const viewport = "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover";
+
 
 export default function Layout({ children }: SidebarProps) {
   return (
--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true,  
+};
 
-module.exports = {
-    images: {
-      domains: ['api.deezer.com', 'e-cdns-images.dzcdn.net', 'images.unsplash.com'],
-    },
-  };
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+module.exports = withPWA({
+  ...nextConfig, // Merge nextConfig with the configurations applied by withPWA
+  images: {
+    domains: ['api.deezer.com', 'e-cdns-images.dzcdn.net', 'images.unsplash.com'],
+  },
+});
