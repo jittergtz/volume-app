325eb27986fd10693c66683de98f70c8212757ea
jittergtz <sandro.gantze@icloud.com>
2024-03-20 20:44:10 +0100

    fix ambient

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,47 +1,41 @@
-import Header from '@/components/Header'
-import ListItem from '@/components/ListItem'
-import MobileNavbar from '@/components/MobileNav'
-import DashboardAmbientLight from '@/components/Next-Ui/dashboardAmbientLight'
+import Header from "@/components/Header"
+import ListItem from "@/components/ListItem"
+import MobileNavbar from "@/components/MobileNav"
+import DashboardAmbientLight from "@/components/Next-Ui/dashboardAmbientLight"
 
-
-
-import React from 'react'
-import Discover from '@/components/Discover'
-import { FaFireFlameCurved } from 'react-icons/fa6'
-import Greeting from '@/components/Greeting'
-import { Tab, Tabs } from '@nextui-org/react'
-
+import React from "react"
+import Discover from "@/components/Discover"
+import { FaFireFlameCurved } from "react-icons/fa6"
+import Greeting from "@/components/Greeting"
+import { Tab, Tabs } from "@nextui-org/react"
 
 function page() {
   return (
-    <main className='bg-white bg-opacity-5  rounded-lg h-full 
-    w-full overflow-hidden overflow-y-auto'>
-   
-      <DashboardAmbientLight/>
+    <main
+      className="bg-white bg-opacity-5 relative  rounded-lg h-full 
+    w-full overflow-hidden overflow-y-auto"
+    >
+      <DashboardAmbientLight />
       <Header>
-        <div className='sm:mb-2'>
-        <Greeting/>
-         
-          <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
-          gap-3 mt-4'>
-            <ListItem 
-            name="Favoriten"
-            image="/image/favoriten-icon.svg"
-             href='/dashboard/playlist/favoriten' />
-          </div>
+        <div className="sm:mb-2">
+          <Greeting />
 
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
+          gap-3 mt-4"
+          >
+            <ListItem
+              name="Favoriten"
+              image="/image/favoriten-icon.svg"
+              href="/dashboard/playlist/favoriten"
+            />
+          </div>
         </div>
-        
       </Header>
 
-
-
-
-
-      <div className='px-5 pb-40'>
+      <div className="px-5 pb-40">
         <Discover />
-    </div>
-  
+      </div>
     </main>
   )
 }
--- components/Next-Ui/dashboardAmbientLight.tsx
+++ components/Next-Ui/dashboardAmbientLight.tsx
@@ -4,7 +4,7 @@ function DashboardAmbientLight() {
   return (
     <>
       <div
-        className="absolute md:flex hidden  left-[300px] rounded-lg w-full   h-48 
+        className="lg:flex absolute inset-0 z-0  hidden  rounded-lg w-full h-48 
        bg-gradient-to-b from-indigo-300/20  "
       ></div>
       <div
