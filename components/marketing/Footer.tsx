0c9456fdaa2b12e52933e081deacc9b66fbda606
Sandro <sandro.gantze@icloud.com>
2024-09-01 20:51:53 +0200

    delete unused stuff

--- components/marketing/Footer.tsx
+++ components/marketing/Footer.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 function Footer() {
   return (
 
-   <footer className='h-20 w-full px-5 bg-neutral-950'>
+   <footer className='h-20 w-full px-5 bg-black'>
     <div className='bg-gradient-to-r from-black via-neutral-400 to-black w-full h-[1px]'></div>
             <div className='flex h-full items-center justify-center'>
             <div className='max-w-5xl text-sm w-full'>
@@ -20,6 +21,12 @@ function Footer() {
          
                </div>
             </div>
+            </div>
+            <div className='flex mt-20 w-full justify-center'>
+            <img 
+            src="/image/VolumeFooterText.svg"
+            alt="logo"
+            className='' />
             </div>
      </footer>
   )
